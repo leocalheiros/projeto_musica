c7347166e23bf71ca3c712a295e452ee53b7e144
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-08-31 15:22:52 -0300

    add project architeture

--- src/models/musica.py
+++
@@ -1,5 +0,0 @@
-class Musica:
-    def __init__(self, titulo, artista, ano):
-        self.titulo = titulo
-        self.artista = artista
-        self.ano = ano
--- src/views/menu_view.py
+++
@@ -1,8 +0,0 @@
-class MenuView:
-    def mostrar_menu(self):
-        print("\n--- Menu ---")
-        print("1. Cadastrar Música")
-        print("2. Buscar Música por Título")
-        print("3. Buscar Músicas por Ano de Publicação")
-        print("4. Criar Playlist Aleatória")
-        print("5. Sair")
