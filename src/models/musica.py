3babc9ff93187ff17b388e5cd94d57bd658e02d7
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-08-31 15:24:21 -0300

    add musica.py in models

---
+++ src/models/musica.py
@@ -0,0 +1,26 @@
+class Musica:
+    def __init__(self, titulo, artista, ano):
+        self.titulo = titulo
+        self.artista = artista
+        self.ano = ano
+
+
+class Playlist:
+    def __init__(self):
+        self.musicas = []
+
+    def adicionar_musica(self, musica):
+        self.musicas.append(musica)
+
+    def mostrar_playlist(self):
+        if not self.musicas:
+            return "Playlist vazia."
+
+        playlist_str = "\n--- Playlist ---\n"
+        for idx, musica in enumerate(self.musicas, start=1):
+            playlist_str += f"{idx}. {musica.titulo} - {musica.artista} ({musica.ano})\n"
+
+        return playlist_str
+
+    def limpar_playlist(self):
+        self.musicas = []
